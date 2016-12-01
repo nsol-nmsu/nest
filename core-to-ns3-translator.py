8105adb22b11c83062d2739b3fc7dcbaf6433f19
Andres <acueva13@nmsu.edu>
2016-11-30 20:21:19 -0700

    updated python script

--- core-to-ns3-translator.py
+++ core-to-ns3-translator.py
@@ -7,7 +7,7 @@ def find(name, path):
     if name in files:
       return os.path.join(root, name)
 
-def addOptions(topo, traceDir, ns2_file, duration, apps):
+def addOptions(topo, traceDir, ns2, duration, apps, pcap):
   run_command = "scratch/core_to_ns3_scenario"
   if topo != "":
     run_command = run_command + " --topo=imn2ns3/" + topo
@@ -15,8 +15,8 @@ def addOptions(topo, traceDir, ns2_file, duration, apps):
     run_command = run_command + " --apps=imn2ns3/" + apps
   if traceDir != "":
     run_command = run_command + " --traceDir=" + traceDir
-  if ns2_file != "":
-    run_command = run_command + " --ns2=imn2ns3/" + ns2_file
+  if ns2 != "":
+    run_command = run_command + " --ns2=imn2ns3/" + ns2
   if duration != "":
     run_command = run_command + " --duration=" + duration
   if pcap != "":
@@ -24,6 +24,8 @@ def addOptions(topo, traceDir, ns2_file, duration, apps):
   run_command = '"' + run_command + '"'
   
   waf_command = './waf --run ' + run_command
+
+  print "\n" + waf_command + "\n"
   return waf_command
       
 def main(argv):
@@ -33,24 +35,26 @@ def main(argv):
   
   topo = ''
   trace = ''
-  ns2_file = ''
+  ns2 = ''
   duration = ''
+  apps = ''
+  pcap = "false"
 
   try:
-    opts, args = getopt.getopt(argv, 'ht:n:d:l', ["help", "topo=", "ns2_file=", "duration=", "trace"])
+    opts, args = getopt.getopt(argv, 'h:t:n:d:l:a:p', ["help", "topo=", "ns2=", "duration=", "trace=", "apps=", "pcap"])
   except getopt.GetoptError:
-    print "xml_to_ns3_pipeline.py --topo/-t=<file.xml> --ns2_file/-n=<file_name> --duration/-d=<time> --trace/-l"
+    print "core-to-ns3-translator.py -t <file/path/in/imn2ns3> -n <file/path/in/imn2ns3> -d <time.float> -l <path/to/folder/> -a <file/path/in/imn2ns3> -p"
     sys.exit(2)
   for opt, arg in opts:
     if opt in ("-h", "--help"):
-      print "xml_to_ns3_pipeline.py -t=<file/path/in/imn2ns3> -n=<file/path/in/imn2ns3> -d=<time.float> -l=<path/to/folder/> -a=<file/path/in/imn2ns3> -p"
+      print "core-to-ns3-translator.py -t <file/path/in/imn2ns3> -n <file/path/in/imn2ns3> -d <time.float> -l <path/to/folder/> -a <file/path/in/imn2ns3> -p"
       sys.exit()
     elif opt in ("-t" ,"--topo"):
       topo = arg
     elif opt in ("-a" ,"--apps"):
       apps = arg
-    elif opt in ("-n", "--ns2_file"):
-      ns2_file = arg
+    elif opt in ("-n", "--ns2"):
+      ns2 = arg
     elif opt in ("-d" ,"--duration"):
       duration = arg
     elif opt in ("-l", "--trace"):
@@ -62,13 +66,13 @@ def main(argv):
     print "There was no xml file given as input, exiting program"
     sys.exit()
   ##print addOptions(topo,trace,ns2_file,duration)
-  os.system(addOptions(topo,trace,ns2_file,duration))
+  os.system(addOptions(topo,trace,ns2,duration,apps,pcap))
 
   ##open netAnim
   f = find("NetAnim", os.path.expanduser('~'))
   path_to_file = f.replace('NetAnim','')
   os.chdir(path_to_file)
-  os.system('./NetAnim')
+  os.system('./NetAnim &')
 
 ##run script 
 if __name__ == "__main__":
@@ -77,17 +81,5 @@ if __name__ == "__main__":
 
 """
 **to generate a scenario, an example syntax would be:**
-./waf --run "scratch/imn_to_ns3_scenario --topo=imn2ns3/imn_sample_files/third.imn"
-
-or
-
-./waf --run "scratch/imn_to_ns3_scenario --topo=imn2ns3/imn_sample_files/sample4-nrlsmf.imn --ns2=imn2ns3/imn_sample_files/sample4.ns_movements --duration=250.0"
-
-or
-
-./waf --run "scratch/xml_to_ns3_scenario --topo=imn2ns3/imn_sample_files/sample1.xml --ns2=imn2ns3/imn_sample_files/sample1.ns_movements --duration=27.0"
-
-or
-
-./waf --run "scratch/core_to_ns3_scenario --topo=imn2ns3/imn_sample_files/WideAreaNetwork3.xml --traceDir=core2ns3_Logs/ --duration=30.0"
+./waf --run "scratch/core_to_ns3_scenario --topo=imn2ns3/CORE-XML-files/sample1.xml --apps=imn2ns3/apps-files/sample1-apps.xml --ns2=imn2ns3/NS2-mobility-files/sample1.ns_movements --duration=27.0 --pcap=true --traceDir=core2ns3_Logs/"
 """
